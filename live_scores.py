b992163ca4d7d54106fcda27dffdf5a9932c9d8d
Bill Wilson <BillWilson@gmail.com@users.noreply.github.com>
2022-06-15 19:24:07 -0500

    New tournament

--- live_scores.py
+++ live_scores.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 class User:
-  def __init__(self, n, one, two, three, ts):
+  def __init__(self, n, one, two, three, four, ts):
     self.name = n
     self.firstPick = one
     self.secondPick = two
     self.thirdPick = three
+    self.fourthPick = four
     self.team_score = ts
 
 class Player:
@@ -16,22 +17,18 @@ class Player:
     self.thru = t
 
 users = []
-users.append(User("Mary Jane","Cameron Smith","Max Homa","Tyrrell Hatton",0))
-users.append(User("Jovon","Justin Thomas","Justin Rose","Sungjae Im",0))
-users.append(User("Jalen","Jon Rahm","Shane Lowry","Marc Leishman",0))
-users.append(User("Quinn","Collin Morikawa","Tommy Fleetwood","Harold Varner III",0))
-users.append(User("Voyles","Brooks Koepka","Sam Burns","Cameron Champ",0))
-users.append(User("Beeb","Xander Schauffele","Patrick Reed","Daniel Berger",0))
-users.append(User("JJ","Scottie Scheffler","Russell Henley","Bryson DeChambeau",0))
-users.append(User("Tyler","Dustin Johnson","Jordan Spieth","Talor Gooch",0))
-users.append(User("Gabe","Tiger Woods","Hideki Matsuyama","Thomas Pieters",0))
-users.append(User("Dylan","Matthew Wolff","Rory McIlroy","Harry Higgs",0))
-users.append(User("Paul","Viktor Hovland","Joaquin Niemann","Patrick Cantlay",0))
-users.append(User("Alek","Tony Finau","Si Woo Kim","Corey Conners",0))
-users.append(User("Bill","Billy Horschel","Abraham Ancer","Matt Fitzpatrick",0))
-users.append(User("Christian","Louis Oosthuizen","Will Zalatoris","Adam Scott",0))
+users.append(User("Paul","Scottie Scheffler","Sam Burns","Tommy Fleetwood","Harold Varner",0))
+users.append(User("Matt","Rory McIlroy","Hideki Matsuyama","Billy Horschel","Justin Rose",0))
+users.append(User("Bill","Cameron Smith","Viktor Hovland","Max Homa","Keegan Bradley",0))
+users.append(User("Beeb","Brooks Koepka","Dustin Johnson","Alex Noren","Cameron Young",0))
+users.append(User("Ramsey","Justin Thomas","Shane Lowry","Talor Gooch","Brian Harman",0))
+users.append(User("Tyler","Jon Rahm","Tony Finau","Daniel Berger","Patrick Reed",0))
+users.append(User("Christian","Xander Schauffele","Sungjae Im","Abraham Ancer","Tyrell Hatton",0))
+users.append(User("JJ","Jordan Spieth","Patrick Cantlay","Corey Conners","Davis Riley",0))
+users.append(User("Quinn","Matt Fitzpatrick","Collin Morikawa","Harris English","Garrick Higgo",0))
+users.append(User("Jovon","Will Zalatoris","Joaquin Niemann","Aaron Wise","Mito Pereira",0))
 
-def get_players(soup, player_col, score_col, thru_col):
+def get_players(soup, player_col, score_col, thru_col, round_one_col):
     rows = soup.find_all("tr", class_="PlayerRow__Overview PlayerRow__Overview--expandable Table__TR Table__even")
     players = []
     for row in rows[0:]:
@@ -43,15 +40,38 @@ def get_players(soup, player_col, score_col, thru_col):
         player = cols[player_col].text.strip()
         score = cols[score_col].text.strip().upper()        
         thru = cols[thru_col].text.strip() if thru_col else "F" 
+        total_score = 0
+        score_found = False
+        x = 0
         if score == 'CUT':
-            score_final = 'CUT'
-            continue
+            while score_found == False:
+                round_score = cols[round_one_col + x].text.strip()
+                total_score = total_score + int(round_score)
+                if (cols[round_one_col + x + 1].text.strip()) == "--":
+                    score_final = total_score - 70 * (x+1)
+                    score_final = score_final + (total_score - 144) * 2
+                    score_found = True
+                x = x + 1
         elif score == 'WD':
-            score_final = 'WD'
-            continue
+            while score_found == False:
+                round_score = cols[round_one_col + x].text.strip()
+                if round_score != "--":
+                    total_score = total_score + int(round_score)
+                    if (cols[round_one_col + x + 1].text.strip()) == "--":
+                        score_final = total_score - 70 * (x+1)
+                        score_found = True
+                    x = x + 1
+                else:
+                    score_final = 0
+                    score_found = True
         elif score == 'DQ':
-            score_final = 'DQ'
-            continue
+            while score_found == False:
+                round_score = cols[round_one_col + x].text.strip()
+                total_score = total_score + int(round_score)
+                if (cols[round_one_col + x + 1].text.strip()) == "--":
+                    score_final = total_score - 70 * (x+1)
+                    score_found = True
+                x = x + 1
         elif score == 'E':
             score_final = 0
         else:
@@ -71,10 +91,12 @@ def get_col_indecies(soup):
     player_fields = ['PLAYER']
     to_par_fields = ['TO PAR', 'TOPAR', 'TO_PAR', 'SCORE']
     thru_fields = ['THRU']
+    round_one_fields = ["R1"]
     header_col = header_rows[0].find_all("th")
     player_col = None
     score_col = None
     thru_col = None
+    round_one_col = None
     for i in range(len(header_col)):
         col_txt = header_col[i].text.strip().upper()
         if col_txt in player_fields:
@@ -86,29 +108,13 @@ def get_col_indecies(soup):
         if col_txt in thru_fields:
             thru_col = i
             continue
+        if col_txt in round_one_fields:
+            round_one_col = i
     if player_col is None or score_col is None:
         print("Unable to track columns")
         exit()
     
-    return player_col, score_col, thru_col
-
-def verify_scrape(players):
-    if len(players) < 25:
-        print("Less than 25 players, seems suspucious, so exiting")
-        exit()
-    bad_entry_count = 0
-    bad_score_count = 0
-    for key, value in players.items():
-        scr = players[key]['TO PAR']
-        if scr == '?':
-            bad_entry_count += 1
-        if type(scr) is int and (scr > 50 or scr < -50):
-            print("Bad score entry, exiting")
-            exit()
-    if bad_entry_count > 3:
-        # arbitrary number here, I figure this is enough bad entries to call it a bad pull
-        print("Multiple bad entries, exiting")
-        exit()
+    return player_col, score_col, thru_col, round_one_col
 
 def get_tournament_name(soup):
     tournament_name = soup.find_all("h1", class_="headline headline__h1 Leaderboard__Event__Title")[0].text
@@ -119,8 +125,8 @@ def get_player_score(name):
     result = requests.get("http://www.espn.com/golf/leaderboard")
     soup = BeautifulSoup(result.text, "html.parser")
 
-    player_col, score_col, thru_col = get_col_indecies(soup)
-    players = get_players(soup, player_col, score_col, thru_col)
+    player_col, score_col, thru_col, round_one_col = get_col_indecies(soup)
+    players = get_players(soup, player_col, score_col, thru_col, round_one_col)
 
     for player in players:
         if(player.name == name):
@@ -132,23 +138,44 @@ def users_scores(name):
     result = requests.get("http://www.espn.com/golf/leaderboard")
     soup = BeautifulSoup(result.text, "html.parser")
 
-    player_col, score_col, thru_col = get_col_indecies(soup)
-    players = get_players(soup, player_col, score_col, thru_col)
+    player_col, score_col, thru_col, round_one_col = get_col_indecies(soup)
+    players = get_players(soup, player_col, score_col, thru_col, round_one_col)
     string_score = ""
+    worst_score = -100
+    worst_player = ""
     for user in users:
-        if user.name == name:
+        if user.name.lower() == name.lower():
             for player in players:
                 if(user.firstPick == player.name):
                     string_score = player.name + " is " + str(player.score) + " thru " + player.thru + "\n"
                     user.team_score = player.score
+                    if player.score > worst_score:
+                        worst_score = player.score
+                        worst_player = player.name
             for player in players:
                 if(user.secondPick == player.name):
                     string_score += player.name + " is " + str(player.score) + " thru " + player.thru + "\n"
                     user.team_score += player.score
+                    if player.score > worst_score:
+                        worst_score = player.score
+                        worst_player = player.name
             for player in players:
                 if(user.thirdPick == player.name):
                     string_score += player.name + " is " + str(player.score) + " thru " + player.thru + "\n"
                     user.team_score += player.score
+                    if player.score > worst_score:
+                        worst_score = player.score
+                        worst_player = player.name
+            for player in players:
+                if(user.fourthPick == player.name):
+                    string_score += player.name + " is " + str(player.score) + " thru " + player.thru + "\n"
+                    user.team_score += player.score
+                    if player.score > worst_score:
+                        worst_score = player.score
+                        worst_player = player.name
+
+            string_score += worst_player + " score is dropped" + "\n"
+            user.team_score = user.team_score - worst_score
     
             string_score += "Your Score is " + str(user.team_score)
 
@@ -159,18 +186,32 @@ def update_scores():
     result = requests.get("http://www.espn.com/golf/leaderboard")
     soup = BeautifulSoup(result.text, "html.parser")
 
-    player_col, score_col, thru_col = get_col_indecies(soup)
-    players = get_players(soup, player_col, score_col, thru_col)
+    player_col, score_col, thru_col, round_one_col = get_col_indecies(soup)
+    players = get_players(soup, player_col, score_col, thru_col, round_one_col)
     for user in users:
+        worst_score = -100
         for player in players:
             if(user.firstPick == player.name):
                 user.team_score = player.score
+                if player.score > worst_score:
+                    worst_score = player.score
         for player in players:
             if(user.secondPick == player.name):
                 user.team_score += player.score
+                if player.score > worst_score:
+                    worst_score = player.score
         for player in players:
             if(user.thirdPick == player.name):
                 user.team_score += player.score
+                if player.score > worst_score:
+                    worst_score = player.score
+        for player in players:
+            if(user.fourthPick == player.name):
+                user.team_score += player.score
+                if player.score > worst_score:
+                    worst_score = player.score
+
+        user.team_score = user.team_score - worst_score
 
 
 def get_leaderboard():
--- main.py
+++ main.py
@@ -9,7 +9,30 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# Stat Bot
+class ServerUser:
+
+    def __init__(self, n):
+        self.name = n
+        self.num_of_messages = 1
+        self.channels = []
+
+    def incrementMessageCount(self):
+        self.num_of_messages = self.num_of_messages + 1
+
+    def addChannel(self,channel_name):
+        self.channels.append(channel_name)
+
+# !Stat Bot
+
 client = discord.Client()
+
+# Stat Bot
+
+serverUsers = []
+serverusersNames = []
+
+# !Stat Bot
 
 @client.event
 async def on_ready():
@@ -20,6 +43,29 @@ async def on_ready():
     print(f'{client.user} is connected to Discord!')
     print(f'{guild.name}(id: {guild.id})')
 
+# Stat Bot 
+""""
+    for server in client.guilds:
+        for channel in server.channels:
+            if str(channel.type) == 'text':
+                print(channel.name)
+                async for msg in channel.history(limit=100000):
+                    if str(msg.author.name) in serverusersNames:
+                        serverusersNames.append(msg.author.name)
+                        for server_user in serverUsers:
+                            if str(msg.author.name) == server_user.name:
+                                server_user.incrementMessageCount()
+                    else:
+                        serverUsers.append(ServerUser(msg.author.name))
+                        serverusersNames.append(msg.author.name)
+
+    serverUsers.sort(key=lambda x: x.num_of_messages)
+
+    for server_user in serverUsers:
+        print(server_user.name + " has said " + str(server_user.num_of_messages) + " messages")
+"""
+# !Stat Bot
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -28,14 +74,14 @@ async def on_message(message):
     if message.content.startswith("Test"):
         await message.channel.send("Message sent in " + message.channel.name + " channel")
 
-    if message.channel.name == "masters-pool-2022":
+    if message.channel.name == "golf-majors-pool" or message.channel.name == "bot-test":
         if message.content.startswith("!"):
             if message.content.startswith("!Leaderboard"):
                 leaderboard_output = ""
                 leaderboard_output = live_scores.get_leaderboard()
                 await message.channel.send(leaderboard_output)
             elif message.content.startswith("!Tiger"):
-                await message.channel.send("Fuck off")
+                await message.channel.send("Not this time bucko")
             else:
                 name = message.content.replace('!','')
                 score_output = live_scores.users_scores(name)
